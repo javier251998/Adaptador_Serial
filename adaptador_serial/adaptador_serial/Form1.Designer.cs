c3df927a20e4ec6cc8e3456f3c66fc9561cc8781
javier251998 <javier4g.dam@gmail.com>
2019-10-06 21:09:20 -0500

    Add files via upload

---
+++ adaptador_serial/adaptador_serial/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace adaptador_serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnBuscarPuertos = new System.Windows.Forms.Button();
+            this.BtnConectar = new System.Windows.Forms.Button();
+            this.BtnEnviarDatos = new System.Windows.Forms.Button();
+            this.CboPuertos = new System.Windows.Forms.ComboBox();
+            this.CboBaudRate = new System.Windows.Forms.ComboBox();
+            this.LblBaudRate = new System.Windows.Forms.Label();
+            this.LblDatosIngreso = new System.Windows.Forms.Label();
+            this.TxtDatos_a_Enviar = new System.Windows.Forms.TextBox();
+            this.TxtDatosRecibidos = new System.Windows.Forms.TextBox();
+            this.SpPuertos = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // BtnBuscarPuertos
+            // 
+            this.BtnBuscarPuertos.Location = new System.Drawing.Point(36, 50);
+            this.BtnBuscarPuertos.Name = "BtnBuscarPuertos";
+            this.BtnBuscarPuertos.Size = new System.Drawing.Size(115, 23);
+            this.BtnBuscarPuertos.TabIndex = 0;
+            this.BtnBuscarPuertos.Text = "BUSCAR PUERTOS";
+            this.BtnBuscarPuertos.UseVisualStyleBackColor = true;
+            this.BtnBuscarPuertos.Click += new System.EventHandler(this.BtnBuscarPuertos_Click);
+            // 
+            // BtnConectar
+            // 
+            this.BtnConectar.Location = new System.Drawing.Point(405, 50);
+            this.BtnConectar.Name = "BtnConectar";
+            this.BtnConectar.Size = new System.Drawing.Size(88, 23);
+            this.BtnConectar.TabIndex = 1;
+            this.BtnConectar.Text = "CONECTAR";
+            this.BtnConectar.UseVisualStyleBackColor = true;
+            this.BtnConectar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnEnviarDatos
+            // 
+            this.BtnEnviarDatos.Location = new System.Drawing.Point(36, 164);
+            this.BtnEnviarDatos.Name = "BtnEnviarDatos";
+            this.BtnEnviarDatos.Size = new System.Drawing.Size(115, 23);
+            this.BtnEnviarDatos.TabIndex = 2;
+            this.BtnEnviarDatos.Text = "ENVIAR DATOS";
+            this.BtnEnviarDatos.UseVisualStyleBackColor = true;
+            this.BtnEnviarDatos.Click += new System.EventHandler(this.BtnEnviarDatos_Click);
+            // 
+            // CboPuertos
+            // 
+            this.CboPuertos.FormattingEnabled = true;
+            this.CboPuertos.Location = new System.Drawing.Point(188, 52);
+            this.CboPuertos.Name = "CboPuertos";
+            this.CboPuertos.Size = new System.Drawing.Size(144, 21);
+            this.CboPuertos.TabIndex = 3;
+            // 
+            // CboBaudRate
+            // 
+            this.CboBaudRate.FormattingEnabled = true;
+            this.CboBaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "56000",
+            "115200"});
+            this.CboBaudRate.Location = new System.Drawing.Point(188, 112);
+            this.CboBaudRate.Name = "CboBaudRate";
+            this.CboBaudRate.Size = new System.Drawing.Size(144, 21);
+            this.CboBaudRate.TabIndex = 4;
+            this.CboBaudRate.Text = "9600";
+            // 
+            // LblBaudRate
+            // 
+            this.LblBaudRate.AutoSize = true;
+            this.LblBaudRate.Location = new System.Drawing.Point(33, 120);
+            this.LblBaudRate.Name = "LblBaudRate";
+            this.LblBaudRate.Size = new System.Drawing.Size(69, 13);
+            this.LblBaudRate.TabIndex = 5;
+            this.LblBaudRate.Text = "BAUD RATE";
+            this.LblBaudRate.Click += new System.EventHandler(this.LblBaudRate_Click);
+            // 
+            // LblDatosIngreso
+            // 
+            this.LblDatosIngreso.AutoSize = true;
+            this.LblDatosIngreso.Location = new System.Drawing.Point(33, 239);
+            this.LblDatosIngreso.Name = "LblDatosIngreso";
+            this.LblDatosIngreso.Size = new System.Drawing.Size(105, 13);
+            this.LblDatosIngreso.TabIndex = 6;
+            this.LblDatosIngreso.Text = "DATOS RECIBIDOS";
+            this.LblDatosIngreso.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtDatos_a_Enviar
+            // 
+            this.TxtDatos_a_Enviar.Location = new System.Drawing.Point(188, 167);
+            this.TxtDatos_a_Enviar.Name = "TxtDatos_a_Enviar";
+            this.TxtDatos_a_Enviar.Size = new System.Drawing.Size(305, 20);
+            this.TxtDatos_a_Enviar.TabIndex = 7;
+            // 
+            // TxtDatosRecibidos
+            // 
+            this.TxtDatosRecibidos.Location = new System.Drawing.Point(188, 232);
+            this.TxtDatosRecibidos.Name = "TxtDatosRecibidos";
+            this.TxtDatosRecibidos.Size = new System.Drawing.Size(305, 20);
+            this.TxtDatosRecibidos.TabIndex = 8;
+            // 
+            // SpPuertos
+            // 
+            this.SpPuertos.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.DatoRecibido);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 310);
+            this.Controls.Add(this.TxtDatosRecibidos);
+            this.Controls.Add(this.TxtDatos_a_Enviar);
+            this.Controls.Add(this.LblDatosIngreso);
+            this.Controls.Add(this.LblBaudRate);
+            this.Controls.Add(this.CboBaudRate);
+            this.Controls.Add(this.CboPuertos);
+            this.Controls.Add(this.BtnEnviarDatos);
+            this.Controls.Add(this.BtnConectar);
+            this.Controls.Add(this.BtnBuscarPuertos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBuscarPuertos;
+        private System.Windows.Forms.Button BtnConectar;
+        private System.Windows.Forms.Button BtnEnviarDatos;
+        private System.Windows.Forms.ComboBox CboPuertos;
+        private System.Windows.Forms.ComboBox CboBaudRate;
+        private System.Windows.Forms.Label LblBaudRate;
+        private System.Windows.Forms.Label LblDatosIngreso;
+        private System.Windows.Forms.TextBox TxtDatos_a_Enviar;
+        private System.Windows.Forms.TextBox TxtDatosRecibidos;
+        private System.IO.Ports.SerialPort SpPuertos;
+    }
+}
+
---
+++ adaptador_serial/adaptador_serial/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace adaptador_serial
+{
+    public partial class Form1 : Form
+    {
+        private delegate void DelegadoAcceso(string accion);
+
+        private string strBufferIn;
+        private string strBufferOut;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AccesoForm(string accion)
+        {
+            strBufferIn = accion;
+            TxtDatosRecibidos.Text = strBufferIn;
+        }
+
+        private void AccesoInterrupcion(string accion)
+        {
+            DelegadoAcceso Var_DelegadoAcceso;
+            Var_DelegadoAcceso = new DelegadoAcceso(AccesoForm);
+            object[] arg = { accion };
+            base.Invoke(Var_DelegadoAcceso,arg);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            strBufferIn = " ";
+            strBufferOut = " ";
+            BtnConectar.Enabled = false;
+            BtnEnviarDatos.Enabled = false;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (BtnConectar.Text=="CONECTAR")
+                {
+                    SpPuertos.BaudRate = Int32.Parse(CboBaudRate.Text);
+                    SpPuertos.DataBits = 8;
+                    SpPuertos.Parity = Parity.None;
+                    SpPuertos.StopBits = StopBits.One;
+                    SpPuertos.Handshake = Handshake.None;
+                    SpPuertos.PortName = CboPuertos.Text;
+
+                    try
+                    {
+                        SpPuertos.Open();
+                        BtnConectar.Text = "DESCONECTAR";
+                        BtnEnviarDatos.Enabled = true;
+                    }
+                    catch(Exception exc)
+                    {
+                        MessageBox.Show(exc.Message.ToString());
+                    }
+                }
+                else if (BtnConectar.Text == "DESCONECTAR")
+                {
+                    SpPuertos.Close();
+                    BtnConectar.Text = "CONECTAR";
+                    BtnEnviarDatos.Enabled = false;
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message.ToString());
+            }
+
+        }
+
+        private void LblBaudRate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBuscarPuertos_Click(object sender, EventArgs e)
+        {
+            string[] PuertosDisponibles = SerialPort.GetPortNames();
+            CboPuertos.Items.Clear();
+
+            foreach(string puerto_simple in PuertosDisponibles)
+            {
+                CboPuertos.Items.Add(puerto_simple);
+            }
+
+            if (CboPuertos.Items.Count > 0)
+            {
+                CboPuertos.SelectedIndex = 0;
+                MessageBox.Show("SELECCIONAR EL PUERTO DE TRABAJO");
+                BtnConectar.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("NINGUN PUERTO DETECTADO");
+                CboPuertos.Items.Clear();
+                CboPuertos.Text = "                    ";
+                strBufferIn = " ";
+                strBufferOut = " ";
+                BtnConectar.Enabled = false;
+                BtnEnviarDatos.Enabled = false;
+            }
+
+        }
+
+        private void BtnEnviarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SpPuertos.DiscardOutBuffer();
+                strBufferOut = TxtDatos_a_Enviar.Text;
+                SpPuertos.Write(strBufferOut);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message.ToString());
+            }
+        }
+
+        private void DatoRecibido(object sender, SerialDataReceivedEventArgs e)
+        {
+            AccesoInterrupcion(SpPuertos.ReadExisting());
+            /*string Data_in = SpPuertos.ReadExisting();
+            MessageBox.Show(Data_in);
+            TxtDatosRecibidos.Text = Data_in;*/
+        }
+    }
+}
